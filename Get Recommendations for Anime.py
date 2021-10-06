a9ecb518e17d9bbf984a17e724357c8552cd42e8
Vephral <litkir20@gmail.com>
2021-10-06 16:07:02 +0800

    rewrite code into script-type

---
+++ Get Recommendations for Anime.py
@@ -0,0 +1,58 @@
+import asyncio
+import pandas as pd
+import jikanpy as jp
+from time import sleep, time
+
+jikan = jp.Jikan()
+anime_dataset = pd.read_csv('./datasets/anime_dataset/anime.csv')
+ids = anime_dataset.MAL_ID.tolist()
+
+def get_cleared_recs(uncleared_recs):
+    titles = []
+    mal_ids = []
+    i = 0
+    while i != 7:
+        try:
+            title = uncleared_recs['recommendations'][i]['title']
+            mal_id = uncleared_recs['recommendations'][i]['mal_id']
+            titles.append(title)
+            mal_ids.append(mal_id)
+            i += 1
+        except IndexError:
+            titles.append('No')
+            mal_ids.append('No')
+            i += 1
+    return titles, mal_ids
+
+def get_recs(id):
+    uncleared_recs = jikan.anime(id, extension='recommendations')
+    recs = get_cleared_recs(uncleared_recs)
+    return recs
+
+anime_recs = pd.DataFrame(columns=['ID', 'Reco_Title', 'Reco_ID'])
+
+start_time = time()
+i = 0
+not_responsed_ids = []
+for item_id in ids:
+    try:
+        if item_id not in anime_recs.ID.tolist():
+            titles, mal_ids = get_recs(item_id)
+            for i in range(7):
+                anime_recs.loc[len(anime_recs.index)] = [item_id, titles[i], mal_ids[i]]
+            if len(anime_recs) % 100 == 0:
+                print(len(anime_recs))
+            sleep(2)
+        if item_id in anime_recs.ID.tolist():
+            continue
+    except jp.APIException:
+        not_responsed_ids.append(item_id)
+        sleep(60)
+print("--- %s seconds ---" % (time() - start_time))
+
+start_time = time()
+anime_recs = asyncio.run(get_recs(ids[:100]))
+print('--- %s seconds ---' % (time() - start_time))
+
+anime_recs.to_csv('C:/Users/ASDW/Python/Projects/Recosys 2.0/datasets/anime_dataset/recommendations.csv')
+print(anime_recs.info())
